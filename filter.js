585afecdd47f84b23299445f0ca8c55e50fbfda3
Rob Davis <bfe5e925@opayq.com>
2022-10-09 19:56:15 +1000

    Fixing modile bug

--- filter.js
+++ filter.js
@@ -1,18 +1,22 @@
-const posts = document.getElementsByTagName("h3");
+function docReady(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
 
 const process = (tags) => {
-    console.log(tags);
+
+    console.log('Running tag filter')
+    const posts = document.getElementsByClassName('PostHeader__post-title-line');
+    console.log(posts.length + ' posts to be checked')
+
     for (let post of posts) {
-        console.log('checking: ' + post.innerHTML);
         tags.forEach(tag => {
             if (post.innerHTML.toLocaleLowerCase().includes(tag.toLocaleLowerCase())) {
                 console.log('Removed one post for containing the word: ' + tag);
                 post.
-                    parentElement.
-                    parentElement.
-                    parentElement.
-                    parentElement.
-                    parentElement.
                     parentElement.
                     parentElement.
                     parentElement.style.display = 'none';
@@ -23,8 +27,29 @@ const process = (tags) => {
 
 }
 
-chrome.storage.sync.get({
-    allTags: [],
-}, (items) => {
-    process(items.allTags);
+docReady(() => {
+    console.log('Document ready, loading tags')
+    chrome.storage.sync.get({
+        allTags: [],
+    }, (items) => {
+        console.log('Tags loaded:' + items.allTags)
+        console.log('Hooking up observer')
+
+        const parent = document.getElementsByClassName("PostsFromSubredditPage")[0]
+
+        console.log('Observing: ' + parent)
+        const config = { childList: true, subtree: true }
+
+        const observer = new MutationObserver((mutationList, observer) => {
+            for (const mutation of mutationList) {
+                if (mutation.type === 'childList') {
+                    console.log('Posts loaded, running tag filter')
+                    process(items.allTags)
+                }
+            }
+        })
+
+        observer.observe(parent, config)
+    });
 });
+
